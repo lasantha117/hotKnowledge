3610d3ea48d4e57ac8ea75fc8928bfd20c802c77
lasantha117 <weerasinghehwlp.20@uom.lk>
2024-07-21 18:39:35 +0530

    Fixed the issue that had with quiz navigation

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -4,15 +4,13 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useColorScheme } from 'react-native';
 import { RiHomeLine, RiDashboardLine, RiExchangeLine, RiGroupLine } from '@remixicon/react';
-import store from '../assets/redux/store';
 import Home from '../components/home/home';
 import Dashboard from '../components/dashbord';
 import SwapPage from '../components/swapPage/swapPage';
 import Friends from '../components/Friends/friends';
+import News from '../components/News';
 import Colors from '../constants/Colors';
 import styles from './bottomNavStyles';
-import News from '../components/News';
-import { Provider } from 'react-redux';
 
 const BottomTab = createBottomTabNavigator();
 
@@ -20,39 +18,37 @@ const BottomTabNavigator = () => {
   const colorScheme = useColorScheme();
 
   return (
-    <Provider store={store}> {/* Wrap the navigator with the Provider */}
-      <BottomTab.Navigator
-        initialRouteName="Home"
-        screenOptions={({ route }) => ({
-          tabBarActiveTintColor: Colors[colorScheme].tint,
-          tabBarStyle: styles.tabBarContainer,
-          tabBarIcon: ({ color, size }) => {
-            let IconComponent;
+    <BottomTab.Navigator
+      initialRouteName="Home"
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: Colors[colorScheme].tint,
+        tabBarStyle: styles.tabBarContainer,
+        tabBarIcon: ({ color, size }) => {
+          let IconComponent;
 
-            if (route.name === 'Home') {
-              IconComponent = RiHomeLine;
-            } else if (route.name === 'Dashboard') {
-              IconComponent = RiDashboardLine;
-            } else if (route.name === 'SwapPage') {
-              IconComponent = RiExchangeLine;
-            } else if (route.name === 'Friends') {
-              IconComponent = RiGroupLine;
-            } else if (route.name === 'News') {
-              IconComponent = RiGroupLine;
-            }
+          if (route.name === 'Home') {
+            IconComponent = RiHomeLine;
+          } else if (route.name === 'Dashboard') {
+            IconComponent = RiDashboardLine;
+          } else if (route.name === 'SwapPage') {
+            IconComponent = RiExchangeLine;
+          } else if (route.name === 'Friends') {
+            IconComponent = RiGroupLine;
+          } else if (route.name === 'News') {
+            IconComponent = RiGroupLine;
+          }
 
-            return <IconComponent size={size} color={color} style={styles.tabBarIcon} />;
-          },
-          headerShown: false, // Ensure the header is not shown
-        })}
-      >
-        <BottomTab.Screen name="Home" component={Home} />
-        <BottomTab.Screen name="Dashboard" component={Dashboard} />
-        <BottomTab.Screen name="SwapPage" component={SwapPage} />
-        <BottomTab.Screen name="Friends" component={Friends} />
-        <BottomTab.Screen name="News" component={News} />
-      </BottomTab.Navigator>
-    </Provider>
+          return <IconComponent size={size} color={color} style={styles.tabBarIcon} />;
+        },
+        headerShown: false, // Ensure the header is not shown
+      })}
+    >
+      <BottomTab.Screen name="Home" component={Home} />
+      <BottomTab.Screen name="Dashboard" component={Dashboard} />
+      <BottomTab.Screen name="SwapPage" component={SwapPage} />
+      <BottomTab.Screen name="Friends" component={Friends} />
+      <BottomTab.Screen name="News" component={News} />
+    </BottomTab.Navigator>
   );
 };
 
--- navigation/index.js
+++ navigation/index.js
@@ -3,17 +3,29 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import BottomTabNavigator from './BottomTabNavigator';
+import Quiz from '../components/quiz/quiz';
 import store from '../assets/redux/store';
+
+const Stack = createStackNavigator();
 
 const linking = {
   prefixes: ['http://localhost:8081'],
   config: {
     screens: {
-      BottomTabs: 'bottom',
+      BottomTabs: {
+        path: 'bottom',
+        screens: {
+          Home: 'home',
+          Dashboard: 'dashboard',
+          SwapPage: 'swap',
+          Friends: 'friends',
+          News: 'news',
+        },
+      },
       Quiz: 'quiz',
-      TelegramWebViewScreen: 'telegram',
     },
   },
 };
@@ -21,7 +33,10 @@ const linking = {
 const Navigation = () => {
   return (
     <NavigationContainer linking={linking}>
-      <BottomTabNavigator />
+      <Stack.Navigator initialRouteName="BottomTabs">
+        <Stack.Screen name="BottomTabs" component={BottomTabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="Quiz" component={Quiz} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
